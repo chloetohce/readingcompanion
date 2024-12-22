e6d95b866f3cec24ba80778487a7a48f06f11e27
chloetohce <chloetohce@gmail.com>
2024-12-22 23:17:40 +0800

    Edited: redis keys

--- src/main/java/sg/edu/nus/iss/readingcompanion/utilities/RedisUtil.java
+++ src/main/java/sg/edu/nus/iss/readingcompanion/utilities/RedisUtil.java
@@ -5,5 +5,5 @@ public class RedisUtil {
 
     public static final String KEY_USERS = "users";
 
-    public static final String KEY_BOOKS = "bookshelf";
+    public static final String KEY_BOOKS = "books";
 }
