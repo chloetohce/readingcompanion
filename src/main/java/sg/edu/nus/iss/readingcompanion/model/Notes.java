350a8cc7ae15147101f8c9eef661add3d160eed4
chloetohce <chloetohce@gmail.com>
2024-12-23 16:44:45 +0800

    Added: Notes model

---
+++ src/main/java/sg/edu/nus/iss/readingcompanion/model/Notes.java
@@ -0,0 +1,28 @@
+package sg.edu.nus.iss.readingcompanion.model;
+
+public class Notes {
+    private String bookId;
+
+    private String text;
+    
+    public Notes() {
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    
+}
